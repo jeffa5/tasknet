e26814928f5ba9929613a0bd84fc75cf71890f7e
Andrew Jeffery <dev@jeffas.io>
2021-01-03 15:35:57 +0000

    Just hide the overflow

--- src/lib.rs
+++ src/lib.rs
@@ -686,7 +686,7 @@ fn view_selected_task(task: &Task, tasks: &HashMap<uuid::Uuid, Task>) -> Node<Ms
                 }
             ],
             div![
-                C!["flex", "flex-row", "overflow-auto"],
+                C!["flex", "flex-row", "overflow-hidden"],
                 tags_suggestions
                     .into_iter()
                     .map(|sug| {
@@ -876,7 +876,7 @@ fn view_text_input(
             }
         ],
         div![
-            C!["flex", "flex-row", "overflow-auto"],
+            C!["flex", "flex-row", "overflow-hidden"],
             suggestions
                 .into_iter()
                 .map(|sug| {
