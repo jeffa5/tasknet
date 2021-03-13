abcd86e2a7ff2ec6c10870be460787edeefdb839
Andrew Jeffery <dev@jeffas.io>
2021-03-13 21:33:35 +0000

    Update cargo and flake

--- flake.nix
+++ flake.nix
@@ -4,28 +4,36 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay }:
-    with import nixpkgs { overlays = [ rust-overlay.overlay ]; system = "x86_64-linux"; };
-    {
-      devShell.x86_64-linux = mkShell {
-        buildInputs = [
-          (rust-bin.nightly.latest.rust.override {
-            extensions = [ "rust-src" ];
-            targets = [ "wasm32-unknown-unknown" ];
-          })
-          cargo-edit
-          cargo-fuzz
-          cargo-make
-          cargo-watch
-          wasm-pack
-          pkgconfig
-          openssl
+  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          overlays = [ rust-overlay.overlay ];
+          system = "x86_64-linux";
+        };
+        rust = pkgs.rust-bin.nightly.latest.rust;
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            (rust.override {
+              extensions = [ "rust-src" ];
+              targets = [ "wasm32-unknown-unknown" ];
+            })
+            cargo-edit
+            cargo-fuzz
+            cargo-make
+            cargo-watch
+            wasm-pack
+            pkgconfig
+            openssl
 
-          rnix-lsp
-          nixpkgs-fmt
-        ];
-      };
-    };
+            rnix-lsp
+            nixpkgs-fmt
+          ];
+        };
+      });
 }
