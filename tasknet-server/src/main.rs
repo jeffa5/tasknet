7d3812a5a19d0a3af7dd8eb13f6beb6fea7c38f9
Andrew Jeffery <dev@jeffas.io>
2021-07-16 09:59:25 +0100

    Broadcast changes from one connection to all

--- tasknet-server/src/main.rs
+++ tasknet-server/src/main.rs
@@ -4,8 +4,13 @@ use std::{
     sync::{Arc, Mutex},
 };
 
-use futures_util::{SinkExt, StreamExt};
-use warp::{addr::remote, Filter};
+use futures_util::{stream::SplitSink, SinkExt, StreamExt};
+use tokio::sync::broadcast;
+use warp::{
+    addr::remote,
+    ws::{Message, WebSocket},
+    Filter,
+};
 
 type MemoryBackend = Arc<
     Mutex<
@@ -22,6 +27,15 @@ fn with_backend(
     warp::any().map(move || backend.clone())
 }
 
+fn with_watch_channel(
+    sender: broadcast::Sender<()>,
+) -> impl warp::Filter<
+    Extract = ((broadcast::Sender<()>, broadcast::Receiver<()>),),
+    Error = Infallible,
+> + Clone {
+    warp::any().map(move || (sender.clone(), sender.subscribe()))
+}
+
 #[tokio::main]
 async fn main() {
     env_logger::init();
@@ -36,6 +50,8 @@ async fn main() {
 
     let backend = Arc::new(Mutex::new(persistent_backend));
 
+    let (sender, _) = broadcast::channel(1);
+
     let routes = warp::get()
         .and(
             warp::path("tasknet").and(warp::fs::dir("tasknet-web/local/tasknet").with(tasknet_log)),
@@ -43,9 +59,13 @@ async fn main() {
         .or(warp::path("sync")
             .and(warp::ws())
             .and(with_backend(backend))
+            .and(with_watch_channel(sender))
             .and(remote())
             .map({
-                move |ws: warp::ws::Ws, backend: MemoryBackend, address: Option<SocketAddr>| {
+                move |ws: warp::ws::Ws,
+                      mut backend: MemoryBackend,
+                      (sender, mut receiver): (broadcast::Sender<()>, broadcast::Receiver<()>),
+                      address: Option<SocketAddr>| {
                     ws.on_upgrade(move |websocket| async move {
                         let (mut tx, mut rx) = websocket.split();
 
@@ -56,47 +76,47 @@ async fn main() {
                         // Send a message to the client first. If they don't have any changes
                         // then their generate_sync_message will be None so they won't have
                         // anything to send.
-                        let message = backend
-                            .lock()
-                            .unwrap()
-                            .generate_sync_message(peer_id.clone())
-                            .unwrap();
-                        if let Some(message) = message {
-                            println!("Sending initial sync message to {:?}", address);
-                            let binary =
-                                Vec::<u8>::from(tasknet_sync::Message::SyncMessage(message));
-                            tx.send(warp::ws::Message::binary(binary)).await.unwrap();
-                        }
+                        send_message(&mut backend, peer_id.clone(), address, &mut tx).await;
 
-                        while let Some(Ok(msg)) = rx.next().await {
-                            if msg.is_binary() {
-                                let bytes = msg.into_bytes();
-                                let message = tasknet_sync::Message::from(bytes);
-                                match message {
-                                    tasknet_sync::Message::SyncMessage(sync_message) => {
-                                        println!("Received sync message from {:?}", address);
-                                        let _patch = backend
-                                            .lock()
-                                            .unwrap()
-                                            .receive_sync_message(peer_id.clone(), sync_message)
-                                            .unwrap();
-                                        let message = backend
-                                            .lock()
-                                            .unwrap()
-                                            .generate_sync_message(peer_id.clone())
-                                            .unwrap();
-                                        if let Some(message) = message {
-                                            let binary = Vec::<u8>::from(
-                                                tasknet_sync::Message::SyncMessage(message),
-                                            );
-                                            tx.send(warp::ws::Message::binary(binary))
-                                                .await
-                                                .unwrap();
+                        loop {
+                            tokio::select! {
+                                Some(Ok(msg)) = rx.next() => {
+                                    if msg.is_binary() {
+                                        let bytes = msg.into_bytes();
+                                        let message = tasknet_sync::Message::from(bytes);
+                                        match message {
+                                            tasknet_sync::Message::SyncMessage(sync_message) => {
+                                                println!("Received sync message from {:?}", address);
+                                                let patch = backend
+                                                    .lock()
+                                                    .unwrap()
+                                                    .receive_sync_message(peer_id.clone(), sync_message)
+                                                    .unwrap();
+
+                                                if patch.is_some() {
+                                                    sender.send(()).unwrap();
+                                                }
+
+                                                send_message(&mut backend, peer_id.clone(), address, &mut tx).await
+                                            }
                                         }
+                                    } else if msg.is_close() {
+                                        println!("close");
+                                        break
+                                    } else {
+                                        println!("unhandled message {:?}", msg);
                                     }
-                                }
+                                },
+
+                                Ok(()) = receiver.recv() => {
+                                    send_message(&mut backend, peer_id.clone(), address, &mut tx).await
+                                },
+
+                                else => break,
                             }
                         }
+
+                        backend.lock().unwrap().reset_sync_state(&peer_id)
                     })
                 }
             })
@@ -105,4 +125,22 @@ async fn main() {
     println!("Serving page on http://localhost:8080/tasknet");
 
     warp::serve(routes).run(([127, 0, 0, 1], 8080)).await;
+}
+
+async fn send_message(
+    backend: &mut MemoryBackend,
+    peer_id: Vec<u8>,
+    address: SocketAddr,
+    tx: &mut SplitSink<WebSocket, Message>,
+) {
+    let message = backend
+        .lock()
+        .unwrap()
+        .generate_sync_message(peer_id.clone())
+        .unwrap();
+    if let Some(message) = message {
+        println!("Sending sync message to {:?}", address);
+        let binary = Vec::<u8>::from(tasknet_sync::Message::SyncMessage(message));
+        let _ = tx.send(warp::ws::Message::binary(binary)).await;
+    }
 }
