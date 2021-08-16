a11e1c6b6120d3d1de401a320f5fa4764b44d208
Andrew Jeffery <dev@jeffas.io>
2021-08-16 21:05:15 +0100

    Update base tables

--- tasknet-server/db/V1__Create_changes_table.sql
+++ tasknet-server/db/V1__Create_changes_table.sql
@@ -1,5 +1,19 @@
-create table CHANGES (
-  DOC_ID int not null,
-  HASH varchar(100) not null,
-  DATA bytea not null
+create table changes (
+  doc_id bytea not null,
+  hash bytea not null,
+  data bytea not null,
+  primary key(doc_id, hash)
+)
+
+create table documents (
+  doc_id bytea not null primary key,
+  heads bytea not null,
+  data bytea not null
+)
+
+create table sync_states (
+  doc_id bytea not null,
+  peer_id bytea not null,
+  data bytea not null,
+  primary key(doc_id, peer_id)
 )
