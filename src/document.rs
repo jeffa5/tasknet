37887dba39b19c574f98ec566764e1f5f4edc243
Andrew Jeffery <dev@jeffas.io>
2022-12-19 11:36:56 +0000

    Add autocommit to document

--- src/document.rs
+++ src/document.rs
@@ -1,13 +1,16 @@
+use automerge::AutoCommit;
 use seed::prelude::{LocalStorage, WebStorage};
 
 use crate::task::Task;
 use std::collections::HashMap;
 
 const TASKS_STORAGE_KEY: &str = "tasknet-tasks";
+const AUTODOC_STORAGE_KEY: &str = "tasknet-autodoc";
 
 #[derive(Debug)]
 pub struct Document {
     pub tasks: HashMap<uuid::Uuid, Task>,
+    pub autodoc: automerge::AutoCommit,
 }
 
 impl Document {
@@ -19,10 +22,15 @@ impl Document {
             }
             Err(_) => HashMap::new(),
         };
-        Self { tasks }
+        let saved_document: Vec<u8> =
+            LocalStorage::get(AUTODOC_STORAGE_KEY).map_or_else(|_| Vec::new(), |bytes| bytes);
+        let autodoc = AutoCommit::load(&saved_document).unwrap_or_else(|_| AutoCommit::new());
+        Self { tasks, autodoc }
     }
 
-    pub fn save(&self) {
+    pub fn save(&mut self) {
         LocalStorage::insert(TASKS_STORAGE_KEY, &self.tasks).expect("save tasks to LocalStorage");
+        LocalStorage::insert(AUTODOC_STORAGE_KEY, &self.autodoc.save())
+            .expect("save autodoc to LocalStorage");
     }
 }
